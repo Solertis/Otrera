855e79489dc970d0b7263ede2981c4fcdb9f4231
AgathaTheWitch <TheWitchAgatha@gmail.com>
2014-07-22 13:23:25 -0400

    Improved content creation tool
    The tool will now append new content
    to category lists. This will help make
    things more searchable. I also made some
    more sample content.
    
    I still need to make a content editor tool
    to allow changes to existing content in
    everything.json. Shouldn't be too difficult.

--- Code/addContent.py
+++ Code/addContent.py
@@ -12,19 +12,23 @@ def pick_content_type():
 	else:
 		return content_type
 
-def get_update(content_type):
+def get_update(content_type, thing_name):
 	template = templates[content_type]
-	new_name = raw_input("Name your %s (lower case only): " % content_type)
-	template[new_name] = template.pop(template.keys()[0])
+	template[thing_name] = template.pop(template.keys()[0])
 	update_data = get_content_info(template, content_type)
 	return update_data
 
 def get_list(key):
-	user_list = raw_input("Give me a comma-separated list (no spaces) for %s: " % key)
+	user_list = raw_input("Give me a comma-separated list for %s: " % key)
 	if len(user_list) == 0:
 		return []
 	else:
-		return user_list.split(",")
+		selected = user_list.split(",")
+		final_list = []
+		for item in selected:
+			item = item.lstrip()
+			final_list.append(item)
+		return final_list
 
 def get_content_info(data_dict, content_type):
 	for k, v in data_dict.iteritems():
@@ -36,14 +40,31 @@ def get_content_info(data_dict, content_type):
 			data_dict[k] = raw_input(k+": ")
 	return data_dict
 
-def make_content(content_type):
+def make_content(content_type, thing_name):
 	with open("data/everything.json") as f:
 		data = json.load(f)
-	update = get_update(content_type)
+	update = get_update(content_type, thing_name)
 	data.update(update)
+	data = update_lists(content_type, data, update, thing_name)
 	with open("data/everything.json","w") as f:
 		json.dump(data, f, sort_keys=True, indent=4, ensure_ascii=False)
 
+def update_lists(content_type, data, update, thing_name):
+	if content_type == "skill":
+		if update[thing_name]["category"] == "class":
+			class_key = update[thing_name]["requirements"]["Class"][0]
+			data[class_key].append(thing_name)
+		elif update[thing_name]["category"] == "attribute":
+			req_key = update[thing_name]["requirements"]["Attributes"][0]
+			att = req_key[:3]
+			req = req_key[3:]
+			data[att][req].append(thing_name)
+	else:
+		key = update[thing_name]["kind"]
+		data[key]["list"].append(thing_name)
+	return data
+
 if __name__=="__main__":
 	content_type = pick_content_type()
-	make_content(content_type)
+	thing_name = raw_input("Name your %s (lower case only): ")
+	make_content(content_type, thing_name)
