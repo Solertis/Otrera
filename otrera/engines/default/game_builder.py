b0d51097cba0c60b02947cd009237bd654b664b4
AgathaTheWitch <TheWitchAgatha@gmail.com>
2014-08-05 17:03:59 -0400

    Stub for Game Builder
    Game builder and game editor may
    be separate things.

---
+++ otrera/engines/default/game_builder.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Program for building games
+# Publishes content to content.json
+
+from content import Content
+from engine import *
+from character import Character
+import json
+
+engine = Content().engine
+
+def get_input(prompt):
+	return raw_input(prompt)
+
+def make_game(input_method):
+	"""
+	Top level method in game builder
+	module for creating new games
+	from scratch.
+	"""
+
+if __name__ == "__main__":
+	make_game()
