8b547e2c73365b710620166f3170e45b1b3a8bf9
AgathaTheWitch <TheWitchAgatha@gmail.com>
2014-08-10 17:24:34 -0400

    Beginning work on Game Builder module
    For now, game builder can just create the
    proper subdirectory and content.json file.
    Going to iterate on this throughout the
    week and hopefully make the 8/15 deadline.

--- otrera/engines/default/game_builder.py
+++ otrera/engines/default/game_builder.py
@@ -7,18 +7,30 @@ from content import Content
 from engine import *
 from character import Character
 import json
+import os
 
 engine = Content().engine
+game = Content().data
 
 def get_input(prompt):
 	return raw_input(prompt)
 
-def make_game(input_method):
+def create_game_directory_and_file(game_name):
+	if os.path.exists("games/"+game_name):
+		print "A game with this name already exists for this engine!!!"
+		exit(1)
+	else:
+		os.makedirs("games/"+game_name)
+		gamefile = open("games/"+game_name+"/content.json","w+")
+
+def make_game(game_name):
 	"""
 	Top level method in game builder
 	module for creating new games
 	from scratch.
 	"""
+	create_game_directory_and_file(game_name)
 
 if __name__ == "__main__":
-	make_game()
+	game_name = get_input("What is the name of your game?: ")
+	make_game(game_name)
