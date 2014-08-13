364f71d0335de46c158a8afed573836bf05139ca
AgathaTheWitch <TheWitchAgatha@gmail.com>
2014-08-12 21:34:29 -0400

    Creating otrera executable
    First pass. Don't have much experience. Will
    need to add flags and support both multiple
    content adding paradigms (interactive vs.
    single commands). This commit puts a foot
    in the door.

--- otrera/game_builder.py
+++ otrera/game_builder.py
@@ -16,12 +16,12 @@ def get_input(prompt):
 	return raw_input(prompt)
 
 def create_game_directory_and_file(game_name):
-	if os.path.exists("games/"+game_name):
+	if os.path.exists("engines/default/games/"+game_name):
 		print "A game with this name already exists for this engine!!!"
 		exit(1)
 	else:
-		os.makedirs("games/"+game_name)
-		gamefile = open("games/"+game_name+"/content.json","w+")
+		os.makedirs("engines/default/games/"+game_name)
+		gamefile = open("engines/default/games/"+game_name+"/content.json","w+")
 
 def make_game(game_name):
 	"""
@@ -31,6 +31,9 @@ def make_game(game_name):
 	"""
 	create_game_directory_and_file(game_name)
 
-if __name__ == "__main__":
+def main():
 	game_name = get_input("What is the name of your game?: ")
 	make_game(game_name)
+
+if __name__ == "__main__":
+	main()
